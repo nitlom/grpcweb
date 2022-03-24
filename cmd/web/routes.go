48868b45ea040bddfb3c97f57b52af1695942100
oz1tmm <martin@oz1tmm.dk>
2022-03-24 15:06:46 +0100

    switched to using go-chi as the router instead of pat

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,13 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/nitlom/webserver/pkg/config"
 	"github.com/nitlom/webserver/pkg/handlers"
 )
 
 func Routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+
+	// mux := pat.New()
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/nitlom/webserver
 go 1.18
 
 require (
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // direct
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	github.com/go-chi/chi v1.5.4
+	github.com/sirupsen/logrus v1.8.1
 )
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
